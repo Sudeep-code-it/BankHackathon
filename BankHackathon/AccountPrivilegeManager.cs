95ffeeedc260c68e0052f9f0b45a70279e7ca0fe
Sudeep A <sudeepma99@gmail.com>
2024-03-20 12:50:24 +0530

    added the AccountPrivilageManager

---
+++ BankHackathon/AccountPrivilegeManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankHackathon
+{
+
+    public enum PrivilegeType
+    {
+         REGULAR,
+         GOLD,
+          PREMIUM
+    }
+    public class AccountPrivilegeManager
+    {
+        private Dictionary<PrivilegeType, double> limitsMap = new Dictionary<PrivilegeType, double>();
+
+        public AccountPrivilegeManager() 
+        {
+
+            using (StreamReader r = new StreamReader("dailyLimits.json"))
+            {
+                string json = r.ReadToEnd();
+                var dailyLimitsConfig = JsonConvert.DeserializeObject<Dictionary<string, double>>(json);
+
+                foreach (var kvp in dailyLimitsConfig)
+                {
+                    PrivilegeType privilegeType;
+                    Enum.TryParse(kvp.Key, out privilegeType);
+                    limitsMap[privilegeType] = kvp.Value;
+                    
+                }
+            }
+
+        }
+        double getDailyLimit(PrivilegeType privilegeType)
+        {
+            if (!Enum.IsDefined(typeof(PrivilegeType), privilegeType))
+                throw new InvalidPrivilegeTypeException("Privilege Type is invalid");
+
+            return limitsMap[privilegeType];
+        }
+    }
+
+   
+}
--- BankHackathon/AllExceptions.cs
+++ BankHackathon/AllExceptions.cs
@@ -43,4 +43,19 @@ namespace BankHackathon
 
 
     }
+
+    public class InvalidPrivilegeTypeException : Exception
+    {
+        public InvalidPrivilegeTypeException()
+        {
+        }
+
+        public InvalidPrivilegeTypeException(string message) : base(message)
+        {
+        }
+
+        public InvalidPrivilegeTypeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
 }
